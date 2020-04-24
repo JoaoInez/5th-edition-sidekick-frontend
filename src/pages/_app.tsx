970eb4add42d2b6429b5cbcbb832cdda07c4dd01
JoaoInez <joaoinez.info@gmail.com>
2020-04-25 00:56:17 +0100

    Changes global css to scss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { AppProps } from "next/app";
-import "../styles/global.css";
+import "../styles/global.scss";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
